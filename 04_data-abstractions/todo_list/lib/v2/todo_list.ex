6cbdd5929373247615a479c39ffe9be49ca60309
Guilherme Camargo <gcamargo@acesso.com>
2022-04-25 13:16:58 -0300

    update_entry

--- 04_data-abstractions/todo_list/lib/v2/todo_list.ex
+++ 04_data-abstractions/todo_list/lib/v2/todo_list.ex
@@ -6,21 +6,32 @@ defmodule V2.TodoList do
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.auto_id)
 
-    new_entries = Map.put(
-      todo_list.entries,
-      todo_list.auto_id,
-      entry
-    )
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.auto_id,
+        entry
+      )
 
-    %V2.TodoList{todo_list |
-      entries: new_entries,
-      auto_id: todo_list.auto_id + 1
-    }
+    %V2.TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
 
   def entries(todo_list, date) do
     todo_list.entries
-    |> Stream.filter(fn {_, entry} -> entry.date == date end) 
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %V2.TodoList{todo_list | entries: new_entries}
+    end
   end
 end
--- 04_data-abstractions/todo_list/test/v2/todo_list_test.exs
+++ 04_data-abstractions/todo_list/test/v2/todo_list_test.exs
@@ -1,6 +1,6 @@
 defmodule V2.TodoListTest do
+  use ExUnit.Case, async: true
 
-  use ExUnit.Case
   alias V2.TodoList
 
   describe "new/0" do
@@ -23,17 +23,17 @@ defmodule V2.TodoListTest do
       new_todo_list = TodoList.add_entry(todo_list, entry)
 
       # assert
-      assert new_todo_list == %V2.TodoList{auto_id: 2, entries: %{1 => %{date: ~D[2000-12-31], id: 1, title: "study"}}}
+      assert new_todo_list == %TodoList{
+               auto_id: 2,
+               entries: %{1 => %{date: ~D[2000-12-31], id: 1, title: "study"}}
+             }
     end
   end
 
   describe "entries/2" do
     test "given a todo_list, should return all entries" do
       # arrange
-      todo_list = TodoList.new()
-      |> TodoList.add_entry(%{date: ~D[2000-12-31], title: "study"})
-      |> TodoList.add_entry(%{date: ~D[2000-12-30], title: "gym"})
-      |> TodoList.add_entry(%{date: ~D[2000-12-30], title: "work"})
+      todo_list = create_fullfil_todolist()
 
       # act
       entries = TodoList.entries(todo_list, ~D[2000-12-30])
@@ -41,5 +41,34 @@ defmodule V2.TodoListTest do
       # assert
       assert Enum.count(entries) == 2
     end
+  end
+
+  describe "update_entry/3" do
+    test "Given a entry_id and a todo_list with entry with the given id, should update the value of the entry and return todo_list" do
+      # arrange
+      todo_list = create_fullfil_todolist()
+      entry_id = 1
+      new_date = ~D[2018-12-20]
+
+      # act
+      new_todolist = TodoList.update_entry(todo_list, entry_id, &Map.put(&1, :date, new_date))
+
+      # assert
+      {_, entry} =
+        Enum.find(
+          new_todolist.entries,
+          nil,
+          fn {id, _entry} -> id == entry_id end
+        )
+
+      assert entry.date == new_date
+    end
+  end
+
+  defp create_fullfil_todolist do
+    TodoList.new()
+    |> TodoList.add_entry(%{date: ~D[2000-12-31], title: "study"})
+    |> TodoList.add_entry(%{date: ~D[2000-12-30], title: "gym"})
+    |> TodoList.add_entry(%{date: ~D[2000-12-30], title: "work"})
   end
 end
