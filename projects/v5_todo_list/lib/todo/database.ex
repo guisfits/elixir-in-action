882181fdb91fd81aaefbf2fd1a068a7a1b5d3da6
Guilherme Camargo <guisfits@hotmail.com>
2022-05-10 13:10:54 -0300

    adding v5 project

---
+++ projects/v5_todo_list/lib/todo/database.ex
@@ -0,0 +1,53 @@
+defmodule Todo.Database do
+  use GenServer
+
+  @db_folder "./persist"
+
+  # * Interfaces
+
+  def start do
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def store(key, data) do
+    GenServer.cast(__MODULE__, {:store, key, data})
+  end
+
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  # * Callbacks
+
+  @impl GenServer
+  def init(_) do
+    File.mkdir_p!(@db_folder)
+    {:ok, nil}
+  end
+
+  @impl GenServer
+  def handle_cast({:store, key, data}, state) do
+    key
+    |> file_name()
+    |> File.write!(:erlang.term_to_binary(data))
+
+    {:no_reply, state}
+  end
+
+  @impl GenServer
+  def handle_call({:get, key}, _, state) do
+    data =
+      case File.read(file_name(key)) do
+        {:ok, contents} -> :erlang.binary_to_term(contents)
+        _ -> nil
+      end
+
+    {:reply, data, state}
+  end
+
+  # * Helpers
+
+  def file_name(key) do
+    Path.join(@db_folder, to_string(key))
+  end
+end
