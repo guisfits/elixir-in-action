425cbe6de09de627fe6a09adb1dd151fc912f2b1
Guilherme Camargo <gcamargo@acesso.com>
2022-06-04 16:50:53 -0300

    v10 project

---
+++ projects/v10_todo_list/test/todo/cache_test.exs
@@ -0,0 +1,31 @@
+defmodule Todo.Cache.Test do
+  use ExUnit.Case
+  alias Todo.Cache
+
+  describe "server_process/2" do
+    test "Given a name, should attach it to some Todo.Server pid" do
+      # arrange
+      {:ok, cache} = Todo.Cache.start_link()
+
+      # act
+      guilherme_pid = Todo.Cache.server_process(cache, "guilherme")
+
+      # assert
+      assert guilherme_pid != Todo.Cache.server_process(cache, "juliana")
+      assert guilherme_pid == Todo.Cache.server_process(cache, "guilherme")
+    end
+
+    test "Given a Todo.Server pid, Should do all Todo.List operations" do
+      # arrange
+      {:ok, cache} = Todo.Cache.start_link()
+      alice = Todo.Cache.server_process(cache, "alice")
+
+      # act
+      Todo.Server.add_entry(alice, %{date: ~D[2018-12-19], title: "Dentist"})
+      entries = Todo.Server.entries(alice, ~D[2018-12-19])
+
+      # assert
+      assert [%{date: ~D[2018-12-19], title: "Dentist"}] = entries
+    end
+  end
+end
