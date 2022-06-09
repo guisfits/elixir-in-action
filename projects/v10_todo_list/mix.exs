f19bf38f8be26f4ce1584c6fab3ef08d2f800d81
Guilherme Camargo <gcamargo@acesso.com>
2022-06-09 09:39:48 -0300

    mix.exs new deps and config

--- projects/v10_todo_list/mix.exs
+++ projects/v10_todo_list/mix.exs
@@ -3,7 +3,7 @@ defmodule TodoList.MixProject do
 
   def project do
     [
-      app: :todo_list,
+      app: :todo,
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
@@ -11,18 +11,18 @@ defmodule TodoList.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Todo.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:poolboy, "~> 1.5"},
+      {:cowboy, "~> 1.1"},
+      {:plug, "~> 1.4"},
     ]
   end
 end
